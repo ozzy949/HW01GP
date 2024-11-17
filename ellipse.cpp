5e51e3b64bf6eaac53697fa0278226db93a68937
ozzy949 <ozzy949@outlook.com>
2024-11-16 20:18:50 -0800

    Create ellipse.cpp

---
+++ ellipse.cpp
@@ -0,0 +1,16 @@
+#include "ellipse.h"
+
+// Example rendering method in your QWidget subclass
+void GraphicsWidget::paintEvent(QPaintEvent* event) {
+    QPainter painter(this);
+
+    // Example pen and brush
+    QPen pen(Qt::blue, 2, Qt::SolidLine);
+    QBrush brush(Qt::yellow, Qt::SolidPattern);
+
+    // Create an Ellipse
+    Ellipse ellipse(1, pen, brush, QPoint(100, 100), QSize(50, 30));
+
+    // Render the Ellipse
+    ellipse.draw(painter);
+}
