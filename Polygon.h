ca02000a517ca9e31dda60ac82a77ed1b61e5c02
melvinle <157773317+melvinle@users.noreply.github.com>
2024-11-20 19:24:05 -0800

    Create Polygon.h

---
+++ Polygon.h
@@ -0,0 +1,29 @@
+#ifndef POLYGON_H
+#define POLYGON_H
+
+#include "Shape.h"
+#include <QVector>
+#include <QPoint>
+
+// Derived class for Polygon
+class Polygon : public Shape {
+public:
+    // Constructor
+    Polygon(int id, const QVector<QPoint>& points, const QPoint& position, 
+            const QPen& pen, const QBrush& brush);
+
+    // Disable copy operations
+    Polygon(const Polygon&) = delete;
+    Polygon& operator=(const Polygon&) = delete;
+
+    // Overridden Virtual Functions
+    void draw(QPainter& painter) const override;
+    void move(const QPoint& newPosition) override;
+    double perimeter() const override;
+    double area() const override;
+
+private:
+    QVector<QPoint> vertices; // Vertices of the polygon relative to the position
+};
+
+#endif // POLYGON_H
